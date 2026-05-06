f6847df5d32c0e9b2fbaa024293cf9c8fa8acd67
JHStudio-dev <jafeth@jhstudio.dev>
2026-05-06 00:35:43 +0000

    fix: prevent background scroll when modal is open on iOS

--- js/app.js
+++ js/app.js
@@ -224,7 +224,7 @@ function openDetail(id) {
 
   // Show
   modal.classList.add('jh-vehicle-modal--active');
-  document.body.style.overflow = 'hidden';
+  lockScroll();
 }
 
 function renderGallery() {
@@ -323,9 +323,29 @@ function handleSwipe() {
   }
 }
 
+function lockScroll() {
+  const scrollY = window.scrollY;
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.left = '0';
+  document.body.style.right = '0';
+  document.body.style.overflow = 'hidden';
+  document.body.dataset.scrollY = scrollY;
+}
+
+function unlockScroll() {
+  const scrollY = parseInt(document.body.dataset.scrollY || '0', 10);
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.left = '';
+  document.body.style.right = '';
+  document.body.style.overflow = '';
+  window.scrollTo(0, scrollY);
+}
+
 function closeModal() {
   modal.classList.remove('jh-vehicle-modal--active');
-  document.body.style.overflow = '';
+  unlockScroll();
 }
 
 document.getElementById('detailClose').addEventListener('click', closeModal);
